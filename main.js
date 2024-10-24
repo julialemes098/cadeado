580ae49511f39a2b3b493a21d74638ce0e11ed33
julialemes098 <103117367+julialemes098@users.noreply.github.com>
2024-10-24 11:30:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const botoes = document.querySelectorAll(‘.parametro-senha__botao’);
+botoes[0].onclick = diminuiTamanho;
+console.log(botoes);
+function diminuiTamanho(){
+}
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+    let tamanhoSenha = 12;
+    function diminuiTamanho(){
+        tamanhoSenha = tamanhoSenha - 1;
+        numeroSenha.textContent = tamanhoSenha
+        }
+        botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+function aumentaTamanho() {
+    tamanhoSenha = tamanhoSenha + 1;
+    numeroSenha.textContent = tamanhoSenha;
+    }
+    function diminuiTamanho() {
+        if (tamanhoSenha > 1)
+        tamanhoSenha = tamanhoSenha - 1;
+        numeroSenha.textContent = tamanhoSenha;
+
+        function aumentaTamanho() {
+            if (tamanhoSenha < 20) {
+            tamanhoSenha = tamanhoSenha + 1;
+            }
+            numeroSenha.textContent = tamanhoSenha;
+        }
+
